7c605d4369b52b66af086869e0eb288acc8a1db8
adnanali000 <adnan.museel123@gmail.com>
2022-03-03 22:32:59 +0500

    home screen header work done

--- screens/Home.js
+++ screens/Home.js
@@ -2,19 +2,80 @@ import React from 'react';
 import {
     View,
     Text,
-    TouchableOpacity
+    TouchableOpacity,
+    SafeAreaView,
+    TouchableWithoutFeedback,
+    Image,
+    ImageBackground,
+    Animated,
+    ScrollView,
+    FlatList,
 } from 'react-native';
+import {dummyData,SIZES,COLORS,icons,images } from '../constants'
+import { EvilIcons } from '@expo/vector-icons';
 
 const Home = ({ navigation }) => {
+
+    function renderHeader(){
+        return(
+            <View style={{
+                flexDirection:'row',
+                justifyContent:'space-between',
+                paddingHorizontal:SIZES.padding,
+                alignItems:'center'
+            }}>
+
+                {/* profile button  */}
+
+                <TouchableOpacity 
+                    style={{
+                        alignItems:'center',
+                        justifyContent:'center',
+                        width:50,
+                        height:50
+                    }}
+                    onPress={()=>alert('Profile')}
+                >
+                   <EvilIcons name="user" size={40} color='red' />
+
+                </TouchableOpacity>
+
+                {/* screen button  */}
+
+                <TouchableOpacity
+                    style={{
+                        alignItems:'center',
+                        justifyContent:'center',
+                        width:50,
+                        height:50
+                    }}
+                    onPress={()=>alert('screen play')}
+                >
+                    <Image 
+                        source={icons.airplay}
+                        style={{
+                            width:25,
+                            height:25,
+                            tintColor:COLORS.primary
+                        }}
+                    />
+
+                </TouchableOpacity>
+
+            </View>
+        )
+    }
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home</Text>
-            <TouchableOpacity
-                onPress={() => navigation.navigate("MovieDetail")}
-            >
-                <Text>Go to MovieDetail</Text>
-            </TouchableOpacity>
-        </View>
+      <SafeAreaView
+        style={{
+            flex:1,
+            backgroundColor:COLORS.black
+        }}
+      >
+          {renderHeader()}
+
+      </SafeAreaView>
     )
 }
 
